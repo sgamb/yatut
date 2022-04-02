579019d523af403f532c1d1b63987e2af810302d
Sergey Gambaryan <sgamb2000@gmail.com>
2022-04-02 12:13:05 +0300

    Add api documentation links

--- yatube/api/urls.py
+++ yatube/api/urls.py
@@ -1,16 +1,55 @@
+from django.conf.urls import url
 from django.urls import include, path
-from rest_framework import routers
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions, routers
 
 from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
 router = routers.DefaultRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'groups', GroupViewSet)
-router.register(r'posts/(?P<post_id>\d+)/comments',
-                CommentViewSet, basename='comments')
-router.register(r'follow', FollowViewSet, basename='followers')
+
+router.register(
+    r'posts',
+    PostViewSet,
+)
+router.register(
+    r'groups',
+    GroupViewSet,
+)
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments',
+)
+router.register(
+    r'follow',
+    FollowViewSet,
+    basename='followers',
+)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
     path('v1/', include('djoser.urls.jwt')),
+]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Yatut API",
+        default_version='v1',
+        description="Документация для приложения api проекта yatut",
+        # terms_of_service="URL страницы с пользовательским соглашением",
+        contact=openapi.Contact(email="sgamb2000@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'),
 ]
