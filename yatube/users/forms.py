88272cce0ef0bbf7ea6f49bb4c6bfa833e45b72a
Sergey Gambaryan <sgamb2000@gmail.com>
2022-04-30 20:39:15 +0300

    Change the error messages on signup template

--- yatube/users/forms.py
+++ yatube/users/forms.py
@@ -7,8 +7,7 @@ User = get_user_model()
 # Create own class for register form
 # And made it the ancestor of the build-in class UserCreationForm
 class CreationForm(UserCreationForm):
+
     class Meta(UserCreationForm.Meta):
-        # Indicate the model to which the form to be created is connected
         model = User
-        # Sepecify which fields shouls be visible in the forn and in what order
         fields = ("first_name", "last_name", "username", "email")
